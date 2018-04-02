14e4576647facefeb0ccc06c8da1b317ddd82968
Thinh Ngo <thinh.ngo@gmail.com>
2018-04-02 12:31:51 -0700

    Refining comment

--- source/js/models/Location.js
+++ source/js/models/Location.js
@@ -6,8 +6,8 @@ export default class Location extends Record({
   lng: 0.0
 }) {
   isAt(otherLocation) {
-    // Method to determine if one location is at the same location as the other.
-    // We may decide to refine this logic at some point to allow two locations to be the
+    // Method to determine if one location is at the same location as another.
+    // We may decide to refine this logic at some point to consider two locations to be the
     // same if they're within a certain distance from one another.
     return this.lat === otherLocation.lat && this.lng === otherLocation.lng;
   }
